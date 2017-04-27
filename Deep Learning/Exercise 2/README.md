1b16756cf26188fbada2b8eefff27a78d36c8979
Rotvig <renerotvig@msn.com>
2017-04-27 14:24:20 +0200

    Exercise 2 added

---
+++ Deep Learning/Exercise 2/README.md
@@ -0,0 +1,3 @@
+# Exercise 1
+Student starter code for the first exercise of Deep Learning Course at Aarhus University.
+The exercises are based on the second and third assignment of the Stanford course CS 231n.
---
+++ Deep Learning/Exercise 2/cs231n/layers.py
@@ -0,0 +1,336 @@
+import numpy as np
+
+def affine_forward(x, w, b):
+  """
+  Computes the forward pass for an affine (fully-connected) layer.
+
+  The input x has shape (N, d_1, ..., d_k) where x[i] is the ith input.
+  We multiply this against a weight matrix of shape (D, M) where
+  D = \prod_i d_i
+
+  Inputs:
+  x - Input data, of shape (N, d_1, ..., d_k)
+  w - Weights, of shape (D, M)
+  b - Biases, of shape (M,)
+  
+  Returns a tuple of:
+  - out: output, of shape (N, M)
+  - cache: (x, w, b)
+  """
+  out = None
+  #############################################################################
+  # TODO: Implement the affine forward pass. Store the result in out. You     #
+  # will need to reshape the input into rows.                                 #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  cache = (x, w, b)
+  return out, cache
+
+
+def affine_backward(dout, cache):
+  """
+  Computes the backward pass for an affine layer.
+
+  Inputs:
+  - dout: Upstream derivative, of shape (N, M)
+  - cache: Tuple of:
+    - x: Input data, of shape (N, d_1, ... d_k)
+    - w: Weights, of shape (D, M)
+
+  Returns a tuple of:
+  - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+  - dw: Gradient with respect to w, of shape (D, M)
+  - db: Gradient with respect to b, of shape (M,)
+  """
+  x, w, b = cache
+  dx, dw, db = None, None, None
+  #############################################################################
+  # TODO: Implement the affine backward pass.                                 #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx, dw, db
+
+
+def relu_forward(x):
+  """
+  Computes the forward pass for a layer of rectified linear units (ReLUs).
+
+  Input:
+  - x: Inputs, of any shape
+
+  Returns a tuple of:
+  - out: Output, of the same shape as x
+  - cache: x
+  """
+  out = None
+  #############################################################################
+  # TODO: Implement the ReLU forward pass.                                    #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  cache = x
+  return out, cache
+
+
+def relu_backward(dout, cache):
+  """
+  Computes the backward pass for a layer of rectified linear units (ReLUs).
+
+  Input:
+  - dout: Upstream derivatives, of any shape
+  - cache: Input x, of same shape as dout
+
+  Returns:
+  - dx: Gradient with respect to x
+  """
+  dx, x = None, cache
+  #############################################################################
+  # TODO: Implement the ReLU backward pass.                                   #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx
+
+def dropout_forward(x, dropout_param):
+  """
+  Performs the forward pass for (inverted) dropout.
+
+  Inputs:
+  - x: Input data, of any shape
+  - dropout_param: A dictionary with the following keys:
+    - p: Dropout parameter. We keep each neuron output with probability p.
+    - mode: 'test' or 'train'. If the mode is train, then perform dropout;
+      if the mode is test, then just return the input.
+    - seed: Seed for the random number generator. Passing seed makes this
+      function deterministic, which is needed for gradient checking but not in
+      real networks.
+
+  Outputs:
+  - out: Array of the same shape as x.
+  - cache: A tuple (dropout_param, mask). In training mode, mask is the dropout
+    mask that was used to multiply the input; in test mode, mask is None.
+  """
+  p, mode = dropout_param['p'], dropout_param['mode']
+  if 'seed' in dropout_param:
+    np.random.seed(dropout_param['seed'])
+
+  mask = None
+  out = None
+
+  if mode == 'train':
+    ###########################################################################
+    # TODO: Implement the training phase forward pass for inverted dropout.   #
+    # Store the dropout mask in the mask variable.                            #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+  elif mode == 'test':
+    ###########################################################################
+    # TODO: Implement the test phase forward pass for inverted dropout.       #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+
+  cache = (dropout_param, mask)
+  out = out.astype(x.dtype, copy=False)
+
+  return out, cache
+
+
+def dropout_backward(dout, cache):
+  """
+  Perform the backward pass for (inverted) dropout.
+
+  Inputs:
+  - dout: Upstream derivatives, of any shape
+  - cache: (dropout_param, mask) from dropout_forward.
+  """
+  dropout_param, mask = cache
+  mode = dropout_param['mode']
+  if mode == 'train':
+    ###########################################################################
+    # TODO: Implement the training phase forward pass for inverted dropout.   #
+    # Store the dropout mask in the mask variable.                            #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                            END OF YOUR CODE                             #
+    ###########################################################################
+  elif mode == 'test':
+    dx = dout
+  return dx
+
+
+def conv_forward_naive(x, w, b, conv_param):
+  """
+  A naive implementation of the forward pass for a convolutional layer.
+
+  The input consists of N data points, each with C channels, height H and width
+  W. We convolve each input with F different filters, where each filter spans
+  all C channels and has height HH and width HH.
+
+  Input:
+  - x: Input data of shape (N, C, H, W)
+  - w: Filter weights of shape (F, C, HH, WW)
+  - b: Biases, of shape (F,)
+  - conv_param: A dictionary with the following keys:
+    - 'stride': The number of pixels between adjacent receptive fields in the
+      horizontal and vertical directions.
+    - 'pad': The number of pixels that will be used to zero-pad the input.
+
+  Returns a tuple of:
+  - out: Output data, of shape (N, F, H', W') where H' and W' are given by
+    H' = 1 + (H + 2 * pad - HH) / stride
+    W' = 1 + (W + 2 * pad - WW) / stride
+  - cache: (x, w, b, conv_param)
+  """
+  out = None
+  #############################################################################
+  # TODO: Implement the convolutional forward pass.                           #
+  # Hint: you can use the function np.pad for padding.                        #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  cache = (x, w, b, conv_param)
+  return out, cache
+
+
+def conv_backward_naive(dout, cache):
+  """
+  A naive implementation of the backward pass for a convolutional layer.
+
+  Inputs:
+  - dout: Upstream derivatives.
+  - cache: A tuple of (x, w, b, conv_param) as in conv_forward_naive
+
+  Returns a tuple of:
+  - dx: Gradient with respect to x
+  - dw: Gradient with respect to w
+  - db: Gradient with respect to b
+  """
+  dx, dw, db = None, None, None
+  #############################################################################
+  # TODO: Implement the convolutional backward pass.                          #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx, dw, db
+
+
+def max_pool_forward_naive(x, pool_param):
+  """
+  A naive implementation of the forward pass for a max pooling layer.
+
+  Inputs:
+  - x: Input data, of shape (N, C, H, W)
+  - pool_param: dictionary with the following keys:
+    - 'pool_height': The height of each pooling region
+    - 'pool_width': The width of each pooling region
+    - 'stride': The distance between adjacent pooling regions
+
+  Returns a tuple of:
+  - out: Output data
+  - cache: (x, pool_param)
+  """
+  out = None
+  #############################################################################
+  # TODO: Implement the max pooling forward pass                              #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  cache = (x, pool_param)
+  return out, cache
+
+
+def max_pool_backward_naive(dout, cache):
+  """
+  A naive implementation of the backward pass for a max pooling layer.
+
+  Inputs:
+  - dout: Upstream derivatives
+  - cache: A tuple of (x, pool_param) as in the forward pass.
+
+  Returns:
+  - dx: Gradient with respect to x
+  """
+  dx = None
+  #############################################################################
+  # TODO: Implement the max pooling backward pass                             #
+  #############################################################################
+  pass
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx
+
+
+def svm_loss(x, y):
+  """
+  Computes the loss and gradient using for multiclass SVM classification.
+
+  Inputs:
+  - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+    for the ith input.
+  - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    0 <= y[i] < C
+
+  Returns a tuple of:
+  - loss: Scalar giving the loss
+  - dx: Gradient of the loss with respect to x
+  """
+  N = x.shape[0]
+  correct_class_scores = x[np.arange(N), y]
+  margins = np.maximum(0, x - correct_class_scores[:, np.newaxis] + 1.0)
+  margins[np.arange(N), y] = 0
+  loss = np.sum(margins) / N
+  num_pos = np.sum(margins > 0, axis=1)
+  dx = np.zeros_like(x)
+  dx[margins > 0] = 1
+  dx[np.arange(N), y] -= num_pos
+  dx /= N
+  return loss, dx
+
+
+def softmax_loss(x, y):
+  """
+  Computes the loss and gradient for softmax classification.
+
+  Inputs:
+  - x: Input data, of shape (N, C) where x[i, j] is the score for the jth class
+    for the ith input.
+  - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    0 <= y[i] < C
+
+  Returns a tuple of:
+  - loss: Scalar giving the loss
+  - dx: Gradient of the loss with respect to x
+  """
+  probs = np.exp(x - np.max(x, axis=1, keepdims=True))
+  probs /= np.sum(probs, axis=1, keepdims=True)
+  N = x.shape[0]
+  loss = -np.sum(np.log(probs[np.arange(N), y])) / N
+  dx = probs.copy()
+  dx[np.arange(N), y] -= 1
+  dx /= N
+  return loss, dx
+
